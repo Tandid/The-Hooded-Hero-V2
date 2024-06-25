d1916119493a38247aa6f7da872ed2a617eee20d
Tandid Alam <tandid.alam@gmail.com>
2024-06-25 19:37:05 -0400

    code cleanup

--- src/game/scenes/Signup.ts
+++ src/game/scenes/Signup.ts
@@ -12,28 +12,14 @@ export default class Signup extends Scene {
 
     create() {
         this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0xff0000);
-
-        this.background = this.add.image(512, 384, "background");
-        this.background.setAlpha(0.5);
-
-        this.SignupText = this.add
-            .text(512, 384, "Signup Here", {
-                fontFamily: "Arial Black",
-                fontSize: 64,
-                color: "#ffffff",
-                stroke: "#000000",
-                strokeThickness: 8,
-                align: "center",
-            })
-            .setOrigin(0.5)
-            .setDepth(100);
+        this.camera.setBackgroundColor(0xfff);
 
         EventBus.emit("current-scene-ready", this);
     }
 
     changeScene() {
-        this.scene.start("MainMenu");
+        // this.scene.start("MainMenu");
+        console.log(this.scene);
     }
 }
 
