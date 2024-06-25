22902399af534e6da53c77fdb1472a81f255f1e0
Tandid Alam <tandid.alam@gmail.com>
2024-06-25 01:29:25 -0400

    Updated Boot Scene with essential items to load

--- src/game/scenes/Boot.ts
+++ src/game/scenes/Boot.ts
@@ -1,22 +1,38 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Boot extends Scene
-{
-    constructor ()
-    {
-        super('Boot');
+export class Boot extends Scene {
+    constructor() {
+        super("Boot");
     }
 
-    preload ()
-    {
+    preload() {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
 
-        this.load.image('background', 'assets/bg.png');
-    }
+        // Load essential assets
+        this.load.image("logo", "public/assets/logo.png");
+        this.load.image("dummy", "public/assets/dummy.png");
+        this.load.image("arrow", "public/assets/weapons/arrow.png");
 
-    create ()
-    {
-        this.scene.start('Preloader');
+        // Add loading bar, progress text, etc.
+        let loadingBar = this.add.graphics({
+            fillStyle: {
+                color: 0xffffff, // white
+            },
+        });
+
+        this.load.on("progress", (percent) => {
+            loadingBar.fillRect(
+                0,
+                this.sys.game.config.height / 2,
+                this.sys.game.config.width * percent,
+                50
+            );
+        });
+
+        this.load.on("complete", () => {
+            this.scene.start("MainMenu");
+        });
     }
 }
+
