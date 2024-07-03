9a2648939784362058affd1dd34ac3d3582d7bf0
Tandid Alam <tandid.alam@gmail.com>
2024-07-02 21:13:20 -0400

    commit for deployment test

--- src/game/preloaders/preloadLevelMaps.ts
+++ src/game/preloaders/preloadLevelMaps.ts
@@ -1,5 +1,5 @@
 export function preloadLevelMaps(scene: Phaser.Scene) {
-    // scene.load.tilemapTiledJSON("level_1", "levels/level_1.json");
+    scene.load.tilemapTiledJSON("level_1", "levels/level_1.json");
     scene.load.tilemapTiledJSON("level_2", "levels/level_2.json");
     scene.load.tilemapTiledJSON("level_3", "levels/level_3.json");
     // scene.load.tilemapTiledJSON("lobby", "levels/waitingScene.json");
